7ea4641d1030f1ada34348014ed7a709bb904fee
Dani Napach <dani@napach.net>
2023-04-29 15:19:08 +0300

    add run as command

--- start-jupyter.cmd
+++ start-jupyter.cmd
@@ -1,6 +1,6 @@
 start cmd /c  jupyter notebook --NotebookApp.notebook_dir="C:\Project\backend\src\models" --NotebookApp.token="Aa123456"
 cd C:\Project\backend\app
-start cmd /c uvicorn main:app --reload
+uvicorn main:app --reload
 cd C:\Project\frontend
 virtualenv env && .\env\Scripts\activate && pip3 install -r requirements.txt && set FLASK_APP=run.py && set FLASK_ENV=development && flask run
 
